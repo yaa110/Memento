66817bf7a7992707af6163e233065a5f145f5e89
Navid <yaa110@gmail.com>
2016-08-24 21:14:48 +0430

    Create Fab class

---
+++ app/src/main/java/github/yaa110/memento/widget/Fab.java
@@ -0,0 +1,46 @@
+package github.yaa110.memento.widget;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+import android.view.View;
+
+import github.yaa110.bilan.App;
+import github.yaa110.bilan.R;
+import github.yaa110.bilan.library.Animator;
+
+public class Fab extends AppCompatImageView {
+	private boolean isHidden = false;
+
+	public Fab(Context context) {
+		super(context);
+	}
+
+	public Fab(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public Fab(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+
+	public void show() {
+		if (isHidden) {
+			isHidden = false;
+			Animator.create(getContext().getApplicationContext())
+				.on(this)
+				.setStartVisibility(View.VISIBLE)
+				.animate(R.anim.fab_scroll_in);
+		}
+	}
+
+	public void hide() {
+		if (App.floating_btn && !isHidden) {
+			isHidden = true;
+			Animator.create(getContext().getApplicationContext())
+				.on(this)
+				.setEndVisibility(View.GONE)
+				.animate(R.anim.fab_scroll_out);
+		}
+	}
+}
