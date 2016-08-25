779a9d01b9863c204631abc6aef061de2f01f773
Navid <yaa110@gmail.com>
2016-08-25 13:56:01 +0430

    Add theme colors to Category

--- app/src/main/java/github/yaa110/memento/model/Category.java
+++ app/src/main/java/github/yaa110/memento/model/Category.java
@@ -1,6 +1,19 @@
 package github.yaa110.memento.model;
 
 public class Category extends DatabaseModel {
+	public static final String[] COLORS = {
+		"#F44336",
+		"#E91E63",
+		"#9C27B0",
+		"#2196F3",
+		"#0097A7",
+		"#00796B",
+		"#4CAF50",
+		"#FFA000",
+		"#FF5722",
+		"#607D8B"
+	};
+
 	public int theme;
 	public int counter;
 }
